488a30c598349994a1804dc770f3dcb0006abbcb
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-07-29 16:38:31 +0530

    Bfs traversal for generating graphs using matplotlib and obtaining values for * avg entropy vs level * max entropy * min entropy * no_of_nodes vs level

--- statistics.py
+++ statistics.py
@@ -1,6 +1,8 @@
 import logging
 import trsl
 import node
+import math
+from matplotlib import pyplot as plt
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
@@ -9,44 +11,53 @@ formatter = logging.Formatter(
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
-entropy_reduction = []
+tree_data = []
 trsl_instance = trsl.Trsl()
 trsl_instance.train()
 
-def avg_entropy_reduction():
+def compute_avg_entropy():
 
     global trsl_instance
 
-    traverse_node(trsl_instance.root)
-    avg_entropy_reduction = (
-        float(reduce(lambda x,y: x + y, entropy_reduction)) / len(entropy_reduction)
+    bfs([trsl_instance.root])
+    plt.xlabel("Level")
+    plt.ylabel("Avg Entropy")
+    plt.plot(range(0,len(tree_data)), map(lambda x: x['avg_entropy'], tree_data))
+    plt.plot(range(0,len(tree_data)), map(lambda x: x['max_entropy'], tree_data))
+    plt.plot(range(0,len(tree_data)), map(lambda x: x['min_entropy'], tree_data))
+    plt.savefig(
+        "Avg Entropy vs Level.png"
     )
-    logging.info("Average Entropy Reduction: %s %%"
-        % (
-            avg_entropy_reduction
-            )
-    )
-    logging.info("Maximum Reduction: %s %%"
-        % (
-            max(entropy_reduction)
-            )
-    )
-    logging.info("Minimum Reduction: %s %%"
-        % (
-            min(entropy_reduction)
-            )
+    plt.figure()
+    plt.xlabel("Level")
+    plt.ylabel("log2(No of Nodes)")
+    plt.plot(range(0,len(tree_data)), map(lambda x: math.log(x['no_of_nodes'],2), tree_data))
+    plt.savefig(
+        "No of Nodes vs Level.png"
     )
 
-def traverse_node(temp):
-    
-    global trsl_instance
-    if temp.rchild is None:
-        entropy_reduction.append(
-            100 * float(trsl_instance.root.entropy - temp.entropy) / trsl_instance.root.entropy
-        )
-        return
-    traverse_node(temp.lchild)
-    traverse_node(temp.rchild)
 
 
-avg_entropy_reduction()
+def bfs(node_list):
+
+    children = []
+    entropy_sum = 0
+    for node in node_list:
+        entropy_sum += node.entropy
+        if node.rchild is not None:
+            children.append(node.rchild)
+        if node.lchild is not None:
+            children.append(node.lchild)
+    tree_data.append(
+        {
+            'avg_entropy': float(entropy_sum) / 2**len(tree_data),
+            'no_of_nodes': len(node_list),
+            'max_entropy': max(node_list, key=lambda x: x.entropy).entropy,
+            'min_entropy': min(node_list, key=lambda x: x.entropy).entropy
+        }
+    )
+    if len(children) is not 0:
+        bfs(children)
+
+
+compute_avg_entropy()
